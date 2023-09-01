734cce24673aa38697a089d171d2046df25417d4
jejejekim <duji1207@gmail.com>
2023-09-02 00:22:12 +0900

    Setting

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hello({ color, name }) {
+    return <div style={{ color }}>안녕하세요 {name}</div>;
+}
+
+Hello.defaultProps = {
+    name: "이름없음",
+    color: "blue",
+};
+
+export default Hello;
